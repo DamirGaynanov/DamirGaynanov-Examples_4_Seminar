51423f07eb2cadbca40783630f3587b2ad39467f
Дамир Гайнанов <damirgaynanov@gmail.com>
2022-10-19 03:54:35 +0700

    Ex_002_First commit

--- Ex_002_Check/Program.cs
+++ Ex_002_Check/Program.cs
@@ -1,3 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Введите число a");
+string a = Console.ReadLine() ?? "";
+int a1= int.Parse(a);
+Console.WriteLine("Введите число b");
+string b = Console.ReadLine() ?? "";
+int b1= int.Parse(b);
 
+if (b1 * b1 == a1)
+    {
+    Console.WriteLine("Число a является квадратом числа b");
+    }
+else
+    {
+    Console.WriteLine("Число a не является квадратом числа b");
+    }
