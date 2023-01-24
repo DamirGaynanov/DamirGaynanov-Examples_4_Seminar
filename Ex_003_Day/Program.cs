bd896874f7ed03d4ab1a3848007fba212aa5033f
_DG_ <damirgaynanov@gmail.com>
2023-01-24 21:02:59 +0400

    Ct Ex_003

--- Ex_003_Day/Program.cs
+++ Ex_003_Day/Program.cs
@@ -1,32 +1,32 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Введите число");
-string a = Console.ReadLine() ?? "";
-int a1 = int.Parse(a);
-else if (a1 == 1)
+string num = Console.ReadLine() ?? "";
+int a = int.Parse(num);
+if (a == 1)
 {
     Console.WriteLine("Monday");
 }
 
-else if(a1 == 2)
+else if  (a == 2)
 {
     Console.WriteLine("Tuesday");
 }
 
-else if (a1 == 3)
+else if  (a == 3)
 {
     Console.WriteLine("Wednesday");
 }
 
-else if (a1 == 4)
+else if  (a == 4)
 {
     Console.WriteLine("Thursday");
 }
 
-else if (a1 == 5)
+else if   (a == 5)
 {
     Console.WriteLine("Friday");
 }
-else 
+else
 {
     Console.WriteLine("Введите число от 1 до 7");
 }
