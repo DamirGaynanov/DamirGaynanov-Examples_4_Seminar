c7451aac8eb552ba41de1139aa6e0f412046908f
_DG_ <damirgaynanov@gmail.com>
2023-01-26 22:06:00 +0400

    Ex_011_Finish

--- Ex_011_Random/Program.cs
+++ Ex_011_Random/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Input data
+
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"{number}");
+int newNum = RemoveSecondDigit(number);
+Console.WriteLine($"{newNum}");
+
+// Метод
+int RemoveSecondDigit(int num)
+{
+    int firstDigit = (num / 100);
+    int thirdDigit = (num % 10);
+    int result = ((firstDigit*10) + thirdDigit);
+    return result;
+}
