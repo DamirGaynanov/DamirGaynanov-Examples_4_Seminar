6d1878d1751ea65478e18d581193dbb2d74a886a
_DG_ <damirgaynanov@gmail.com>
2023-02-04 01:57:31 +0400

    Done

--- Ex_022/Program.cs
+++ Ex_022/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+Console.Write("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+TabQuarter(number);
+
+
+
+// Метод
+void TabQuarter(int num)
+{
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count}   {count * count}");
+        count++;
+        // count = count + 1;
+    }
+}
