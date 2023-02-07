3974dc4f5d9bca56761dbc1b6a42389003b16b6b
_DG_ <damirgaynanov@gmail.com>
2023-02-07 23:14:34 +0400

    Done

--- Ex_026/Program.cs
+++ Ex_026/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 26: 
+// Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+
+// Например:
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число");
+int number = Convert.ToInt32 (Console.ReadLine());
+// Console.WriteLine(number/10);
+int numbersOfDigit = NumbersOfDigit(number);
+Console.WriteLine($"Количество цифр в числе {number} -> {numbersOfDigit}");
+// Метод
+int NumbersOfDigit(int num)
+{
+    int amount = 0;
+    for (int i = 1; num > 0; i++)
+    {
+        num = num / 10;
+        amount = i;
+    }
+    return amount;
+}
---
+++ Ex_028/Program.cs
@@ -0,0 +1,40 @@
+﻿// Задача 28: 
+// Напишите программу, которая принимает на вход число N
+// и выдаёт произведение чисел от 1 до N.
+// 
+// Например:
+// 4 -> 24
+// 5 -> 120
+// ___________________
+
+Console.WriteLine("Введите число");
+int number;
+if (int.TryParse(Console.ReadLine(), out number))
+{
+    if (number > 0)
+        {
+            int factorial = Factorial(number);
+            Console.WriteLine(factorial);
+        }
+    else
+        {
+            Console.WriteLine("Введено некорректное значение. Введите натуральное число");
+        }
+}
+else
+    {
+        Console.WriteLine("Введено некорректное значение. Введите натуральное число");
+    }
+
+
+
+// Метод
+int Factorial(int num)
+{
+    int result = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
