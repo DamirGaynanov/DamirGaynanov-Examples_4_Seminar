28c47224f99af12d38e13f6582bac31bc40a6097
_DG_ <damirgaynanov@gmail.com>
2023-03-01 13:26:23 +0300

    Done

--- Ex_030/Program.cs
+++ Ex_030/Program.cs
@@ -11,18 +11,18 @@ PrintArray(arr);
 
 int[] CreateArray(int size)
 {
-    int[] array = new int [size];
+    int[] array = new int[size];
     for (int i = 0; i < size; i++)
     {
-        array[i] = new Random().Next(0,2);
+        array[i] = new Random().Next(0, 2);
     }
     return array;
 }
 
 void PrintArray(int[] array)
 {
-    for (int i = 0; i < array.Length ; i++)
+    for (int i = 0; i < array.Length; i++)
     {
-        Console.Write ($"{array[i]} ");
+        Console.Write($"{array[i]} ");
     }
 }
